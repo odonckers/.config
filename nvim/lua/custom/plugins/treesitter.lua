3578a4189f80983d583d2033234ed4369a0897e2
Owen Donckers <odonckers@gmail.com>
2026-01-18 10:16:47 -0500

    added mise treesitter support

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -28,4 +28,11 @@ return {
             additional_vim_regex_highlighting = false,
         },
     },
+    init = function()
+        require('vim.treesitter.query').add_predicate('is-mise?', function(_, _, bufnr, _)
+            local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+            local filename = vim.fn.fnamemodify(filepath, ':t')
+            return string.match(filename, '.*mise.*%.toml$') ~= nil
+        end, { force = true, all = false })
+    end,
 }
