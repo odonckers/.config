06b52cef79e52d748595d32b1ae8899e79ec3973
Owen Donckers <odonckers@jw.org>
2026-01-20 09:18:38 -0500

    updated obsidian config

--- nvim/lua/custom/plugins/obsidian.lua
+++ nvim/lua/custom/plugins/obsidian.lua
@@ -16,24 +16,40 @@ return {
             },
         },
     },
+    cmd = 'Obsidian',
     keys = {
         -- Top level commands
-        { '<leader>nn', '<cmd>Obsidian new<cr>', desc = 'New note' },
-        { '<leader>ns', '<cmd>Obsidian search<cr>', desc = 'Search notes' },
-        { '<leader>nw', '<cmd>Obsidian workspace<cr>', desc = 'Switch workspaces' },
+        { '<leader>n<leader>', '<cmd>Obsidian quick_switch<cr>', desc = 'Quick switch', silent = true },
+        { '<leader>nn', '<cmd>Obsidian new<cr>', desc = 'New note', silent = true },
+        { '<leader>ns', '<cmd>Obsidian search<cr>', desc = 'Search notes', silent = true },
+        { '<leader>nw', '<cmd>Obsidian workspace<cr>', desc = 'Switch workspaces', silent = true },
 
         -- Note commands
-        { '<leader>nb', '<cmd>Obsidian backlinks<cr>', desc = 'Goto backlinks' },
-        { '<leader>nc', '<cmd>Obsidian toggle_checkbox<cr>', desc = 'Cycle checkbox under cursor' },
-        { '<leader>nI', '<cmd>Obsidian paste_img<cr>', desc = 'Paste image at cursor' },
-        { '<leader>nl', '<cmd>Obsidian links<cr>', mode = 'n', desc = 'Links in note' },
-        { '<leader>no', '<cmd>Obsidian follow_link<cr>', desc = 'Open/follow link under cursor' },
-        { '<leader>nr', '<cmd>Obsidian rename<cr>', desc = 'Rename note or link under cursor' },
-        { '<leader>nt', '<cmd>Obsidian toc<cr>', desc = 'Search table of contents for note' },
+        { '<leader>nb', '<cmd>Obsidian backlinks<cr>', desc = 'Goto backlinks', silent = true },
+        { '<leader>nc', '<cmd>Obsidian toc<cr>', desc = 'Search table of contents for note', silent = true },
+        { '<leader>nd', '<cmd>Obsidian dailies<cr>', desc = 'Search daily notes', silent = true },
+        { '<leader>nI', '<cmd>Obsidian paste_img<cr>', desc = 'Paste image at cursor', silent = true },
+        { '<leader>nl', '<cmd>Obsidian links<cr>', mode = 'n', desc = 'Links in note', silent = true },
+        { '<leader>no', '<cmd>Obsidian follow_link<cr>', desc = 'Open/follow link under cursor', silent = true },
+        { '<leader>nr', '<cmd>Obsidian rename<cr>', desc = 'Rename note or link under cursor', silent = true },
+        { '<leader>nt', '<cmd>Obsidian today<cr>', desc = 'Open todays note', silent = true },
+        { '<leader>ny', '<cmd>Obsidian yesterday<cr>', desc = 'Open yesterdays note', silent = true },
 
         -- Visual mode commands
-        { '<leader>ne', '<cmd>Obsidian extract_note<cr>', mode = 'v', desc = 'Extract selection into new note' },
-        { '<leader>nl', '<cmd>Obsidian link<cr>', mode = 'v', desc = 'Link selection to an existing note' },
-        { '<leader>nL', ':Obsidian link_new ', mode = 'v', desc = 'Link selection to a new note' },
+        {
+            '<leader>ne',
+            '<cmd>Obsidian extract_note<cr>',
+            mode = 'v',
+            desc = 'Extract selection into new note',
+            silent = true,
+        },
+        {
+            '<leader>nl',
+            '<cmd>Obsidian link<cr>',
+            mode = 'v',
+            desc = 'Link selection to an existing note',
+            silent = true,
+        },
+        { '<leader>nL', ':Obsidian link_new ', mode = 'v', desc = 'Link selection to a new note', silent = true },
     },
 }
