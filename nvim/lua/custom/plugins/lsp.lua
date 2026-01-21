96ceb68179da5f35724410c00df9b5f1ab6c5cb0
Owen Donckers <odonckers@jw.org>
2026-01-21 15:29:05 -0500

    replaced blink-cmp with nvim-cmp

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -2,16 +2,90 @@ return {
     'neovim/nvim-lspconfig',
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
-        { 'folke/lazydev.nvim', ft = 'lua', config = true }, -- Lua Neovim APIs
-        { 'GustavEikaas/easy-dotnet.nvim', dependencies = { 'nvim-lua/plenary.nvim' } }, -- C# / .NET
-        { 'antosha417/nvim-lsp-file-operations', dependencies = { 'nvim-lua/plenary.nvim' } }, -- Filetree LSP operations
-        'stevearc/conform.nvim', -- Formatting
+        -- Completion sources
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'f3fora/cmp-spell',
+        'hrsh7th/nvim-cmp',
+
+        -- Snippet collections
+        'rafamadriz/friendly-snippets',
+
+        -- Language support
+        { 'folke/lazydev.nvim', ft = 'lua', config = true },
+        { 'GustavEikaas/easy-dotnet.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+        { 'antosha417/nvim-lsp-file-operations', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+        -- Formatting
+        'stevearc/conform.nvim',
     },
     config = function()
+        -- Disable builtin completion keys
+        vim.keymap.set({ 'i', 's' }, '<C-p>', '<nop>')
+        vim.keymap.set({ 'i', 's' }, '<C-n>', '<nop>')
+
+        -- Setup completion
+        local cmp = require('cmp')
+        cmp.setup({
+            snippet = {
+                expand = function(args) vim.snippet.expand(args.body) end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-n>'] = cmp.mapping.select_next_item(),
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'easy-dotnet' },
+                { name = 'path' },
+                { name = 'buffer' },
+                { name = 'spell' },
+            }),
+        })
+
+        -- Use buffer source for `/` and `?` (if you enabled `view.entries = 'native'`, this won't work anymore).
+        cmp.setup.cmdline({ '/', '?' }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' },
+            },
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `view.entries = 'native'`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' },
+            }, {
+                { name = 'cmdline' },
+            }),
+        })
+
+        -- Setup LSP file operations
         require('lsp-file-operations').setup()
+
+        -- Setup language servers
+        local capabilities = vim.tbl_deep_extend(
+            'force',
+            vim.lsp.protocol.make_client_capabilities(),
+            require('cmp_nvim_lsp').default_capabilities()
+        )
 
         -- LuaLS/lua-language-server
         vim.lsp.config('lua_ls', {
+            capabilities = capabilities,
             on_init = function(client)
                 if client.workspace_folders then
                     local path = client.workspace_folders[1].name
@@ -41,11 +115,21 @@ return {
         })
         vim.lsp.enable('lua_ls')
 
-        -- npm:vscode-langservers-extracted
+        -- HTML (npm:vscode-langservers-extracted)
+        vim.lsp.config('html', { capabilities = capabilities })
         vim.lsp.enable('html')
+
+        -- CSS (npm:vscode-langservers-extracted)
+        vim.lsp.config('cssls', { capabilities = capabilities })
         vim.lsp.enable('cssls')
+
+        -- JSON (npm:vscode-langservers-extracted)
+        vim.lsp.config('jsonls', { capabilities = capabilities })
         vim.lsp.enable('jsonls')
+
+        -- ESLint (npm:vscode-langservers-extracted)
         vim.lsp.config('eslint', {
+            capabilities = capabilities,
             settings = {
                 useESLintClass = false,
                 experimental = { useFlatConfig = true },
@@ -53,19 +137,26 @@ return {
         })
         vim.lsp.enable('eslint')
 
-        -- npm:@vtsls/language-server
+        -- TypeScript / JavaScript (npm:@vtsls/language-server)
+        vim.lsp.config('vtsls', { capabilities = capabilities })
         vim.lsp.enable('vtsls')
 
-        -- npm:@angular/language-server
+        -- Angular (npm:@angular/language-server)
+        vim.lsp.config('angularls', { capabilities = capabilities })
         vim.lsp.enable('angularls')
 
-        -- dotnet
-        require('easy-dotnet').setup()
+        -- Setup dotnet
+        vim.lsp.config('easy_dotnet', { capabilities = capabilities })
+        local easy_dotnet = require('easy-dotnet')
+        easy_dotnet.setup()
+        cmp.register_source('easy-dotnet', easy_dotnet.package_completion_source)
 
-        -- npm:@github/copilot-language-server
+        -- GitHub Copilot (npm:@github/copilot-language-server)
+        vim.lsp.config('copilot', { capabilities = capabilities })
         vim.lsp.enable('copilot')
 
-        -- macOS:SourceKit (for swift)
+        -- Swift (macOS:SourceKit)
+        vim.lsp.config('sourcekit', { capabilities = capabilities })
         vim.lsp.enable('sourcekit')
 
         -- Formatting
