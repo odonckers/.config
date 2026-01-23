6f080bb4a1d43d513041a2fc220ac817d9b18ace
Owen Donckers <odonckers@gmail.com>
2026-01-23 15:31:21 -0500

    added lazy context

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,38 +1,46 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'windwp/nvim-ts-autotag' },
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
-    opts = {
-        ensure_installed = {
-            'c',
-            'c_sharp',
-            'lua',
-            'vim',
-            'vimdoc',
-            'regex',
-            'lua',
-            'bash',
-            'markdown',
-            'markdown_inline',
-            'javascript',
-            'typescript',
-            'angular',
-            'kdl',
-            'xml',
-            'yaml',
-            'dockerfile',
+    {
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = { 'windwp/nvim-ts-autotag' },
+        build = ':TSUpdate',
+        main = 'nvim-treesitter.configs',
+        opts = {
+            ensure_installed = {
+                'c',
+                'c_sharp',
+                'lua',
+                'vim',
+                'vimdoc',
+                'regex',
+                'lua',
+                'bash',
+                'markdown',
+                'markdown_inline',
+                'javascript',
+                'typescript',
+                'angular',
+                'kdl',
+                'xml',
+                'yaml',
+                'dockerfile',
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
         },
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
+        init = function()
+            require('vim.treesitter.query').add_predicate('is-mise?', function(_, _, bufnr, _)
+                local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+                local filename = vim.fn.fnamemodify(filepath, ':t')
+                return string.match(filename, '.*mise.*%.toml$') ~= nil
+            end, { force = true, all = false })
+        end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {
+            mode = 'topline',
         },
     },
-    init = function()
-        require('vim.treesitter.query').add_predicate('is-mise?', function(_, _, bufnr, _)
-            local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
-            local filename = vim.fn.fnamemodify(filepath, ':t')
-            return string.match(filename, '.*mise.*%.toml$') ~= nil
-        end, { force = true, all = false })
-    end,
 }
