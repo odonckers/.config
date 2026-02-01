cab12b6419658cdf348cfc2ca8d1e44bef1dc8be
Owen Donckers <odonckers@gmail.com>
2026-02-01 15:09:39 -0500

    updated aerospace config and removed move mini commands

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -1,9 +1,5 @@
 local version = false
 return {
-    -- Text editing
-    { 'nvim-mini/mini.move', version = version, config = true }, -- Move any selection in any direction
     { 'nvim-mini/mini.surround', version = version, config = true }, -- Surround actions
-
-    -- Appearance
     { 'nvim-mini/mini.icons', version = version, config = true }, -- Icon provider
 }
