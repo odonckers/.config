947b931dcfc96a8068de9c5df9e163ea5d0ce0a5
Owen Donckers <odonckers@gmail.com>
2026-02-01 19:20:26 -0500

    was not using brew-install

--- zsh/functions/brew.zsh
+++ zsh/functions/brew.zsh
@@ -8,28 +8,6 @@ brew-restore() {
   brew bundle install --global
 }
 
-brew-install() {
-  if [ -z "$1" ]; then
-      echo "Usage: brew-install [options] <formula|cask> [...]"
-      echo ""
-      return 1
-  fi
-
-  brew install $1
-  brew-backup
-}
-
-brew-uninstall() {
-  if [ -z "$1" ]; then
-      echo "Usage: brew-uninstall [options] <installed_formula|installed_cask> [...]}"
-      echo ""
-      return 1
-  fi
-
-  brew uninstall $1
-  brew-backup
-}
-
 brew-update() {
   brew update
   brew bundle upgrade --global
